9602e01c752f9c778a1822b3656dc971e89186cd
Dean Francis Casili <deancasili@Dean-MBP.local>
2025-05-09 22:03:38 +0800

    trying to fix an error in deployment

--- backend/src/index.js
+++ backend/src/index.js
@@ -31,9 +31,9 @@ app.use("/api/messages", messageRoutes);
 if (process.env.NODE_ENV === "production") {
   app.use(express.static(path.join(__dirname, "../frontend/dist")));
 
-  app.get("*", (req, res) => {
+  app.get("/{*any}", (req, res) => {
     res.sendFile(path.join(__dirname, "../frontend", "dist", "index.html"));
-  })
+  });
 }
 
 // used this server from socket.js
